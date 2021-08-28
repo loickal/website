320414b56885fd3e123459db84286f160a2dc804
iPommes <pommes2303@gmail.com>
2021-08-28 12:36:13 +0200

    Fixed bug in Cookie and switched to localstorage

--- assets/modules/cookie/cookie.js
+++ assets/modules/cookie/cookie.js
@@ -1,23 +1,21 @@
 const cookieBox = document.querySelector(".cookiewrapper"),
 acceptBtn = cookieBox.querySelector("button");
 
-
-let checkCookie = document.cookie.indexOf("AcceptedMhamCookie=True"); //checking our cookie
-//if cookie is set then hide the cookie box else show it
-if(checkCookie != -1){
-  cookieBox.classList.add("hide") 
-}else{
+if(localStorage.getItem("cookie_accepted") == null){
   cookieBox.style.opacity = 1;
   cookieBox.classList.remove("hide");
+}else{
+  cookieBox.classList.add("hide") 
 }
 
 
 acceptBtn.onclick = ()=>{
   document.cookie = "AcceptedMhamCookie=True max-age="+60*60*24*30;
+  localStorage.setItem("cookie_accepted", true);
   if(document.cookie){ //if cookie is set
     cookieBox.classList.add("hide"); //hide cookie box
     cookieBox.style.opacity = 0;
   }else{ //if cookie not set then alert an error
-    alert("Hmm... Looks like you don't want that, the cookies are with you! Please enable cookies!");
+    alert("Hmm... Looks like you don't want that the cookies are with you! Please enable cookies!");
   }
 }
