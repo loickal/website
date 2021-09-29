1ffcbd3448747133d8252492d5556c2c437f2509
iPommes <pommes2303@gmail.com>
2021-09-29 18:34:36 +0200

    Make the website simpler - Added a direct link to the latest download - Added header and footer in the latest page

--- changelog/latest/latestFetcher.js
+++ changelog/latest/latestFetcher.js
@@ -14,17 +14,29 @@ fetch("/changelog/data/json/changelog.json")
 
 
         document.getElementsByClassName("hero")[0].remove();
-        var mainobject = document.getElementsByClassName("mainwrapper")[0];
+        var mainobject = document.getElementsByClassName("infocontent")[0];
 
         if (mainobject != null) {
             if (latestObject == null) {
-                createElementWithContent("hero", mainobject, "div", `<h1 class="hero__title" style="font-size: 350%; color: var(--accent-color);">Error</h1>
+                createElementWithContent("hero", mainobject, "div", `
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <h1 class="hero__title" style="font-size: 350%; color: var(--accent-color);">Error</h1>
                 <p class="hero__description">Couldn't fetch the latest build!</p>
                 <p class="hero__description">Please contact us on <a href="https://discord.gg/2yDWH3VxKC">Discord</a> or try again later...</p>`)
                 return;
             }
 
-            createElementWithContent("hero", mainobject, "div", `<h1 class="hero__title" style="font-size: 350%;">Downloading latest...</h1>
+            createElementWithContent("hero", mainobject, "div", `
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <h1 class="hero__title" style="font-size: 350%;">Downloading latest...</h1>
             <p class="hero__description">Your download should be started in 5 seconds!</p>
             <p class="hero__description">Or <a href="'/changelog/data/downloads/` + latestObject["download"] + `">click here!</a></p>
             <p class="hero__description">Experience issues? Javascript enabled? Try to refresh this site or contact us on <a href="https://discord.gg/2yDWH3VxKC">Discord</a></p>`)
